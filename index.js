15963fdc5e69b04ac007c4ff913b54cac6558113
彭振 <pengzhen@zhigui.com>
2022-01-09 14:02:42 +0800

    refactor: made output clearer in index.js

--- index.js
+++ index.js
@@ -6,59 +6,59 @@ const r1 = new RangeList();
 r1.add([1,5])
 output = r1.print() 
 if ('[1 5)' === output) {
-  console.log(output)
+  console.log('add([1,5]) => ', output)
 }
 
 r1.add([10,20])
 output = r1.print() 
 if ('[1 5) [10 20)' === output) {
-  console.log(output)
+  console.log('add([10,20]) => ', output)
 }
 
 r1.add([20,20])
 output = r1.print() 
 if ('[1 5) [10 20)' === output) {
-  console.log(output)
+  console.log('add([20,20]) => ', output)
 }
 
 r1.add([20,21])
 output = r1.print() 
 if ('[1 5) [10 21)' === output) {
-  console.log(output)
+  console.log('add([20,21]) => ', output)
 }
 
 r1.add([2,4])
 output = r1.print() 
 if ('[1 5) [10 21)' === output) {
-  console.log(output)
+  console.log('add([2,4]) => ', output)
 }
 
 r1.add([3,8])
 output = r1.print() 
 if ('[1 8) [10 21)' === output) {
-  console.log(output)
+  console.log('add([3,8]) => ', output)
 }
 
 r1.remove([10,10])
 output = r1.print() 
 if ('[1 8) [10 21)' === output) {
-  console.log(output)
+  console.log('remove([10,10]) => ', output)
 }
 
 r1.remove([10,11])
 output = r1.print() 
 if ('[1 8) [11 21)' === output) {
-  console.log(output)
+  console.log('remove([10,11]) => ', output)
 }
 
 r1.remove([15,17])
 output = r1.print() 
 if ('[1 8) [11 15) [17 21)' === output) {
-  console.log(output)
+  console.log('remove([5,17]) => ', output)
 }
 
 r1.remove([3,19])
 output = r1.print() 
 if ('[1 3) [19 21)' === output) {
-  console.log(output)
+  console.log('remove([3,19]) => ', output)
 }
